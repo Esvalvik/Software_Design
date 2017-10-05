a96eb17cc69021c9d6ca4d0719da4872cc9eea08
Esvalvik <esv_esv@hotmail.com>
2017-10-05 17:35:37 +0200

    Endret namespace til Snake istedet for SnakeMess, og laget en ny klasse Snake

--- PG3300_Innlevering_Kode/SnakeMess/Snake.cs
+++ PG3300_Innlevering_Kode/SnakeMess/Snake.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 
-namespace SnakeMess
+namespace Snake
 {
-
 	public class Snake
 	{
+		List<Vector2> Body = new List<Vector2>();
+		Vector2 Head { get; }
+		Vector2 Tail { get; }
 
-		public Snake()
+		public Snake(int size)
 		{
+			Head = Body.First();
+			Tail = Body.Last();
+
+			for(int i = 0; i < size; i++)
+			{
+				Body.add(new Vector2(10, 10));
+			}
 		}
 	}
 }
---
+++ PG3300_Innlevering_Kode/SnakeMess/SnakeGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake
+{
+	public class SnakeGame
+	{
+		public static void Main(string arg[])
+		{
+			GameHandler Snake = new GameHandler();
+		}
+	}
+}
